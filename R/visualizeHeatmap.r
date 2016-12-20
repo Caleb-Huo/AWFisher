0ae67a0eff203b34713bee8d0e8e67a49208d0ae
Caleb <xiaoguang1988@gmail.com>
2016-12-19 22:00:51 -0500

    visualization dissimilarity matrix

---
+++ R/visualizeHeatmap.r
@@ -0,0 +1,78 @@
+##' Visualize dissimilarity heatmap
+##'
+##' Visualize dissimilarity heatmap
+##' @title Visualize dissimilarity heatmap
+##' @param dissimilarity Input dissimilarity matrix
+##' @param ddr Input dendrogram
+##' @param clusterMembership clusterMembership result from previous cut tree
+##' @param ... Other parameters for heatmap.2
+##' @return heatmap.2 object
+##' @author Caleb
+##' @export
+##' @examples
+##' N0 = 10
+##' G <- 1000
+##' GDEp <- 50
+##' GDEn <- 50
+##' K = 4
+##'
+##' studies <- NULL
+##' set.seed(15212)
+##' for(k in 1:K){
+##' 	astudy <- matrix(rnorm(N0*2*G),nrow=G,ncol=N0*2)
+##' 	ControlLabel <- 1:N0
+##' 	caseLabel <- (N0 + 1):(2*N0)
+##'
+##' 	astudy[1:GDEp,caseLabel] <- astudy[1:GDEp,caseLabel] + 2
+##' 	astudy[1:GDEp + GDEn,caseLabel] <- astudy[1:GDEp,caseLabel] - 2
+##'
+##' 	alabel = c(rep(0,length(ControlLabel)),rep(1,length(caseLabel)))
+##'
+##' 	studies[[k]] <- list(data=astudy, label=alabel)
+##' }
+##'
+##'
+##' result <- biomarkerCategorization(studies,function_limma,B=100,DEindex=NULL,seed = 15213)
+##' dissimilarity <- result$dissimilarity
+##' ddr <- hclustTree(dissimilarity)
+##' clusterMembership <- cutTree(ddr)
+##' visualizeTree(ddr, clusterMembership)
+##'
+##'
+visualizeHeatmap <- function(dissimilarity, ddr, clusterMembership,...){
+	rainbow_fun <- function(n, c=90, l=50, ...) {
+	if(requireNamespace("colorspace")) {
+	colorspace::rainbow_hcl(n, c = c, l = l, ...)
+		} else {
+			rainbow(n, ...)
+		}
+	}
+
+	reorderLabel <- function(alabel,aorder){
+	  uniLab = unique(alabel)
+	  resLabel = NULL
+	  for(i in 1:length(uniLab)){
+	    resLabel[uniLab[i]==alabel]=aorder[i]
+	  }
+	  return(resLabel)
+	}
+
+	anOrder <- order.dendrogram(ddr)
+	dissimilarity_order <- dissimilarity[anOrder,anOrder]
+
+	clusterMembership_order <- clusterMembership[order.dendrogram(ddr)]
+
+	colorBar <- NULL
+	membershipTable <- table(clusterMembership_order)
+	n <- length(membershipTable)
+	allColor <- rainbow_fun(n)
+	coll0 <- allColor[clusterMembership_order]
+	coll <- reorderLabel(coll0, allColor)
+	#allColor[n] <- '#777777'
+
+	my_palette <- colorRampPalette(c("yellow2", "black", "skyblue"))(n = 300)
+
+	heatmap.2(dissimilarity_order,ColSideColors = coll,col=my_palette, trace="none",Rowv=NA,Colv=NA,key=FALSE,...)
+
+}
+
